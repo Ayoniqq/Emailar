a92c8c8845d2f0025461c55dabc7702de1eb9424
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-06 10:42:39 +0100

    Emailar

--- server.js
+++ server.js
@@ -16,13 +16,23 @@ let transporter = nodemailer.createTransport({
         refreshToken: process.env.OAUTH_REFRESH_TOKEN
     }
 });
+//NODEMAILER MAIL OPTIONS
+let mailOptions = {
+    from: 'ayorindeadeshina@gmail.com',
+    to: 'ayuspee@gmail.com',
+    subject: 'Emailar(Nodemailer Project)',
+    text: 'This is a message from Nodemailer Project - EMAILAR'
+};
 
-
-
-
-
-
-
+//NODEMAILER SEND MAIL
+transporter.sendMail(mailOptions, function(err,data){
+    if(err){
+        console.log('Error' + err);
+    }
+    else{
+        console.log('Email Sent Successully');
+    }
+});
 
 app.listen(PORT, (req,res) => {
     console.log(`SERVER ON PORT: ${PORT}`);
