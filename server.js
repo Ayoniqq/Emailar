805fab7c47c0de3878845dd911a04921ff90fb51
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-06 09:52:06 +0100

    Nodemailer added

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+//const nodeMailer = require('nodemailer');
+
+
+//NODEMAILER TRANSPORTER OBJECT
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+});
+
+
+
+
+
+
+
 
 app.listen(PORT, (req,res) => {
     console.log(`SERVER ON PORT: ${PORT}`);
-    
 })
+
+
+
